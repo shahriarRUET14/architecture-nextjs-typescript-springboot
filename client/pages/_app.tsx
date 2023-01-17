714fe19dd2a9d68f97c2ca3cd51c9d220feb98f9
mahmud <shahriar.ruet14@gmail.com>
2023-01-18 00:10:13 +0600

    restart

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,10 +1,6 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { AppProvider } from "../components/context/AppProvider";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-  <Component {...pageProps} />
-  );
-};
-
+  return <Component {...pageProps} />
+}
